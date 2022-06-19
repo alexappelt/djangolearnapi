e1e626465c03d65c52b924e33edd7a52c71f8ae1
Alex <alexappelt6501@gmail.com>
2022-06-19 19:28:34 -0300

    finishing the API Project

--- escola/views.py
+++ escola/views.py
@@ -1,6 +1,26 @@
-from django.http import JsonResponse
+from rest_framework import viewsets, generics
+from escola.models import Aluno, Curso, Matricula
+from escola.serializer import AlunoSerializer, CursoSerializer, MatriculaSerializer, ListaMatriculasAlunoSerializer
 
-def alunos(request):
-    if request.method == 'GET':
-        aluno = {'id': 1, 'nome': 'Alex'}
-        return JsonResponse(aluno)
+
+class AlunosViewSet(viewsets.ModelViewSet):
+    """Exibindo todos os alunos e alunas"""
+    queryset = Aluno.objects.all()
+    serializer_class = AlunoSerializer
+
+class CursosViewSet(viewsets.ModelViewSet):
+    """Exibindo todos os cursos"""
+    queryset = Curso.objects.all()
+    serializer_class = CursoSerializer
+
+class MatriculasViewSet(viewsets.ModelViewSet):
+    """Exibindo todas as matriculas"""
+    queryset = Matricula.objects.all()
+    serializer_class = MatriculaSerializer
+
+class ListaMatriculasAluno(generics.ListAPIView):
+    """Listando as matrículas de um aluno"""
+    def get_queryset(self):
+        queryset = Matricula.objects.filter(aluno_id=self.kwargs['pk'])
+        return queryset
+    serializer_class = ListaMatriculasAlunoSerializer
